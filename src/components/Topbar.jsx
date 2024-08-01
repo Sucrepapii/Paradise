f3eb42a7aef99118fb31ed94627cc3b788511b6d
Samuel Akinboro <akinboroo@gmail..com>
2024-08-01 14:43:11 +0100

    commit

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-import {AiFillPhone, AiOutlineClockCircle} from 'react-icons/ai'
-import {BsChatSquareDots} from 'react-icons/bs'
+import React from "react";
+import { AiFillPhone, AiOutlineClockCircle } from "react-icons/ai";
+import { BsChatSquareDots } from "react-icons/bs";
 
-const TopBar = () =>{
+const TopBar = () => {
   return (
-    <div>TopBar</div>
-  )
-}
+    <div className="flex justify-between items-center px-4 py-2">
+      <div className="flex items-center">
+        <BsChatSquareDots
+          size={30}
+          className="text-[var(--primary-dark)] mr-2 "/>
+        <h1 className="text-xl font-bold text-gray-700">WEEKAWAY</h1>
+      </div>
+      <div className="flex ">
+      <div className="hidden md:flex items-center px-6">
+        <AiOutlineClockCircle size={20} className="mr-2 text-[var(--primary-dark)]" />
+        <p className="text-sm text-gray-700">9AM - 5PM</p>
+      </div>
+      <div className="hidden md:flex items-center px-6">
+        <AiFillPhone size={20} className="mr-2 text-[var(--primary-dark)]" />
+        <p className="text-sm text-gray-700">+234-902-4129891</p>
+      </div>
+      <button>Get a free Quote</button>
+      </div>
+    </div>
+  );
+};
 
-export default TopBar
+export default TopBar;
