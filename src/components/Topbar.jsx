1d346e4635b3389970aaecdb473a782efd1b6ddd
Samuel Akinboro <akinboroo@gmail..com>
2024-08-01 12:39:51 +0100

    first commit

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {AiFillPhone, AiOutlineClockCircle} from 'react-icons/ai'
+import {BsChatSquareDots} from 'react-icons/bs'
+
+const TopBar = () =>{
+  return (
+    <div>TopBar</div>
+  )
+}
+
+export default TopBar
